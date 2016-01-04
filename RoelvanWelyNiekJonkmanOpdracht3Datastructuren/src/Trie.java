c855ca165067c732d3628ea86c32216fd7b66f09
Roel958 <roelvanwely958@gmail.com>
2016-01-04 12:47:10 +0100

    Trienode iets

--- RoelvanWelyNiekJonkmanOpdracht3Datastructuren/src/Trie.java
+++ RoelvanWelyNiekJonkmanOpdracht3Datastructuren/src/Trie.java
@@ -1,4 +1,43 @@
+public class Trie<T> {
 
-public class Trie {
+	private TrieNode<T> root;
+
+	public Trie() {
+		root = new TrieNode<T>();
+	}
+	
+	
+	/**
+	 * Voegt een woord aan de Trie toe.
+	 * @param word Woord om toe te voegen
+	 */
+	public void insert(String word, T data) {
+		if(word != null && !word.isEmpty() && word.matches("[a-zA-Z]+")) {
+			root.insert(word.toLowerCase(), data);
+		}
+	}
+	
+	
+	/**
+	 * Doorzoekt de Trie met een bepaalde prefix
+	 * @param prefix TrieNode prefix
+	 * @return De data van de gevonden TrieNode, of null
+	 */
+	public T search(String prefix) {
+		TrieNode<T> lastNode = root;
+		
+		// Proberen een node te vinden van de laatste letter van de prefix
+		for(int i = 0; i < prefix.length(); i++) {
+			lastNode = lastNode.findNode(prefix.substring(i, i+1));
+			
+			// Indien er geen node gevonden is: null terug geven
+			if(lastNode == null) {
+				return null;
+			}
+		}
+		
+		return lastNode.getData();
+	}
+	
 
 }
