828ede8efd1e61d0b12b9ca340624ba3f2166553
Niek Jonkman <karigbroodjekaas>
2016-01-07 23:43:07 +0100

    WoordToevoegen bijna klaar

--- RoelvanWelyNiekJonkmanOpdracht3Datastructuren/src/TrieNode.java
+++ RoelvanWelyNiekJonkmanOpdracht3Datastructuren/src/TrieNode.java
@@ -46,6 +46,36 @@ public class TrieNode<T> {
 	}
 	
 	/**
+	 * Kijkt of er een kind is die het karakter ook heeft
+	 * Als dat zo is geeft deze die terug 
+	 * anders null
+	 * @param subKarakter
+	 * @return
+	 */
+	public TrieNode<T> getKindMetKarakter(String subKarakter){
+		for(TrieNode<T> kind : kinderen){
+			if(kind.getKarakter() == subKarakter){
+				return kind;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Kijkt of er nog kinderen zijn, als dat zo is
+	 * dan ben je geen blad, anders wel
+	 * @return
+	 */
+	public boolean controleerBlad(){
+		if(kinderen.size() > 0){
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	
+	/**
 	 * Gaat een woord toevoegen aan tree/trie
 	 * @param woord het woord dat wordt toegevoegd 
 	 * @param data de extra data die wordt toegevoegd
@@ -53,21 +83,15 @@ public class TrieNode<T> {
 	public void woordToevoegen(String woord,T data){
 		
 		//Check of het niet t laatste node is van een woord
-		if(kinderen.size() > 0){
-			isBlad = false;
-		}
+		isBlad = controleerBlad();
 		
 		TrieNode<T> kindMetKarakter = null;
 		String subKarakter = woord.substring(0,1);
 		
 		//Kijk of er kinderen zijn die het karakter al bevatten
 		//Als dat zo is dan sla je de node van dat kind op
-		for(TrieNode<T> kind : kinderen){
-			if(kind.getKarakter() == subKarakter){
-				kindMetKarakter = kind;
-			}
-		}
-		
+		kindMetKarakter = getKindMetKarakter(subKarakter);
+	
 		//Als het karakter nog niet bekend is en je bent bij een blad, dan wordt het karakter toegevoegd
 		//Als het karakter nog niet bekend is maar je bent niet bij een blad
 		//maak dan een nieuwe node met de data en karakter(s)
@@ -77,6 +101,10 @@ public class TrieNode<T> {
 			kindMetKarakter = new TrieNode<T>(data,subKarakter);
 			kindMetKarakter.ouder = this;
 			kinderen.add(kindMetKarakter);
+		}else if(kindMetKarakter != null && karakter.length() > 1){
+			//het kind met karakter moet nu alleen komen te staan
+			//en de rest van de karakters worden een nieuwe node
+			
 		}
 		
 		//Als het woord nog een lengte heeft meer dan 1 dan zijn
