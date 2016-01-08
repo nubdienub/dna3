35fcbfc12c3adcab78e25aa587bf57016e9ead0f
Niek Jonkman <karigbroodjekaas>
2016-01-08 21:00:07 +0100

    Search afgemaakt, trie toegepast, insert werkt voor 90% maar mist nog het veranderen van de locatie van de data

--- RoelvanWelyNiekJonkmanOpdracht3Datastructuren/src/Apl.java
+++ RoelvanWelyNiekJonkmanOpdracht3Datastructuren/src/Apl.java
@@ -3,16 +3,20 @@ public class Apl {
 
 	public static void main(String[] args) {
 		TrieNode<String> root = new TrieNode<String>();
-		
-		root.woordToevoegen("ball", "1");
-		root.woordToevoegen("bat", "2");
-		root.woordToevoegen("doll", "3");
-		root.woordToevoegen("dork", "4");
-		root.woordToevoegen("do", "5");
-		root.woordToevoegen("dorm", "6");
-		root.woordToevoegen("send", "7");
-		root.woordToevoegen("sense", "8");
+			
+		root.insert("ball", "het einde van ball");
+		root.insert("bat", "het einde van bat");
+		root.insert("doll", "het einde van doll");
+		root.insert("dork", "het einde van dork");
+		root.insert("do", "het einde van do");
+		root.insert("dorm", "het einde van dorm");
+		root.insert("send", "het einde van send");
+		root.insert("sense", "het einde van sense");
 
+		
+		System.out.println("Ik zoek data van bat: " + root.search("bat"));
+		System.out.println("Ik zoek data van dorm: " + root.search("dorm"));	
+		
 		System.out.println(root.toDOTString());
 	}
 
--- RoelvanWelyNiekJonkmanOpdracht3Datastructuren/src/TrieNode.java
+++ RoelvanWelyNiekJonkmanOpdracht3Datastructuren/src/TrieNode.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
-public class TrieNode<T> {
+public class TrieNode<T> implements Trie<Object> {
 
 	private boolean isBlad,isRoot;
 	private String karakter = "";
-	private T data;
+	private ArrayList<Object> data;
 	private TrieNode<T> ouder;
 	private ArrayList<TrieNode<T>> kinderen;
 		
@@ -23,18 +23,18 @@ public class TrieNode<T> {
 	 * @param data de data die er bij hoort
 	 * @param karakter de karakter die er bij hoort
 	 */
-	public TrieNode(T data,String karakter) {
+	public TrieNode(String karakter) {
 		this.kinderen = new ArrayList<TrieNode<T>>();
 		this.karakter = karakter;
 		this.isBlad = true;
-		this.data = data;	
+		this.data = new ArrayList<Object>();
 	}
 	
 	/**
 	 * Geeft de data terug 
 	 * @return data
 	 */
-	public T getData(){
+	public Object getData(){
 		return this.data;
 	}
 	
@@ -79,45 +79,10 @@ public class TrieNode<T> {
 		}
 	}
 	
-	
 	/**
-	 * Gaat een woord toevoegen aan tree/trie
-	 * @param woord het woord dat wordt toegevoegd 
-	 * @param data de extra data die wordt toegevoegd
+	 * Deze methode (van Evert) geeft een DOTString terug
+	 * deze kan je gebruiken om je trie te testen
 	 */
-	public void woordToevoegen(String woord,T data){
-		
-		isBlad = controleerBlad();
-
-		TrieNode<T> kindMetKarakter = null;
-		String subKarakter = woord.substring(0,1);
-
-		kindMetKarakter = getKindMetKarakter(subKarakter);
-		
-		if(kindMetKarakter == null && isBlad ){
-			karakter += subKarakter;	
-			this.data = data;
-		}else if(kindMetKarakter == null){
-			kindMetKarakter = new TrieNode<T>(data,subKarakter);
-			kindMetKarakter.ouder = this;
-			kinderen.add(kindMetKarakter);
-		}else if(kindMetKarakter != null && kindMetKarakter.karakter.length() > 1){
-			TrieNode<T> gespletenKindMetKarakter = new TrieNode<T>(data,kindMetKarakter.karakter.substring(1, kindMetKarakter.karakter.length()));
-			kindMetKarakter.karakter = subKarakter;
-			kindMetKarakter.kinderen.add(gespletenKindMetKarakter);
-			gespletenKindMetKarakter.ouder = kindMetKarakter;
-		}
-		
-		if(woord.length() > 1){
-			if(kinderen.size() > 0){
-				kindMetKarakter.woordToevoegen(woord.substring(1), data);
-			}else{
-				woordToevoegen(woord.substring(1), data);
-			}
-		}		
-	}
-	
-	
 	public String toDOTString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("diagraph mijngraaf {\n");
@@ -148,58 +113,85 @@ public class TrieNode<T> {
 		
 		return sb.toString();
 	}
-	
-	
-	
-	
 
-	
 	/**
-	 * Verwijder methode die eerst checkt of deze node kinderen heeft
-	 * zo ja: haal elk kind op en maak deze null (maak isBlad als kinderen.size 0 is)
-	 * zo nee: is het een blad? Vraag het dus aan zijn parent (1tje omhoog) (recursief)
-	 * @param s > te verwijderen woord
+	 * Voegt woorden toe aan de trie structuur
+	 * LET OP: Voegt nog niet data toe op de correcte manier, op het moment voegt die data toe bij het goede element
+	 * op het moment van aanmaken, echter wanneer een element aangepast wordt en dus de data op een andere plek moet staan
+	 * blijft deze op de oude plek staan.
+	 * @param word het woord waar dat we in de trie willen stoppen
+	 * @param data het object dat we bij het woord willen opslaan
 	 */
-	public void woordVerwijderen(String s){
-		System.out.println("Te verwijderen: " + s);
-		// Zoeken naar het te verwijderen character
-		if(kinderen.size() > 0){ 
-			for(int i = 0; i < kinderen.size(); i++) {
-				TrieNode<T> kind = kinderen.get(i);
-				if(kind != null && kind.kinderen.equals(s)) {
-					System.out.println("kind " + s + " wordt null!");
-					kind = null;
-					kinderen.remove(i);
-					isBlad = kinderen.size() == 0;
-					break;
-				}
-				
-				//TODO iets met isWord hier nog?
+	public void insert(String word, Object data) {
+		
+		isBlad = controleerBlad();
+
+		TrieNode<T> kindMetKarakter = null;
+		String subKarakter = word.substring(0,1);
+
+		kindMetKarakter = getKindMetKarakter(subKarakter);
+		
+		if(kindMetKarakter == null && isBlad ){
+			karakter += subKarakter;	
+		}else if(kindMetKarakter == null){
+			kindMetKarakter = new TrieNode<T>(subKarakter);
+			kindMetKarakter.ouder = this;
+			kinderen.add(kindMetKarakter);
+		}else if(kindMetKarakter != null && kindMetKarakter.karakter.length() > 1){
+			TrieNode<T> gespletenKindMetKarakter = new TrieNode<T>(kindMetKarakter.karakter.substring(1, kindMetKarakter.karakter.length()));
+			kindMetKarakter.karakter = subKarakter;
+			kindMetKarakter.kinderen.add(gespletenKindMetKarakter);
+			gespletenKindMetKarakter.ouder = kindMetKarakter;
+		}
+		
+		if(word.length() > 1){
+			if(kinderen.size() > 0){
+				kindMetKarakter.insert(word.substring(1), data);
+			}else{
+				insert(word.substring(1), data);
+			}
+		}else{
+			if(kinderen.size() > 0){
+				getKindMetKarakter(subKarakter).data.add(data);
+			}else{
+				this.data.add(data);
 			}
 		}
 		
-		// Indien blad, vraag parent om zich te verwijderen
-		if(isBlad) {
-			ouder.woordVerwijderen(karakter);
-		}
+	}
 
-	}
 	/**
-	 * Zoekt s in de kinderen van de huidige node, zodra deze s gelijk is aan karakter van kind,
-	 * return dit kind (node)
-	 * @param s
-	 * @return het kind
+	 * Zoekt het data object dat bij een woord hoort 
+	 * en geeft dit terug
+	 * LET OP: Houd geen rekening met dubbele woorden! (bijvoorbeeld 2x bal)
 	 */
-	public TrieNode<T> vindNode(String s) {
-		for(TrieNode<T> kind : kinderen) {
-			if(s != null && kind.karakter != null && s.equals(kind.karakter)) {
-				return kind;
-			}
-		}
-		return null;
+	public ArrayList<Object> search(String word) {		
+		if(word.length() > 0){
+			try {
+				String subKarakter = word.substring(0,1);
+				TrieNode<T> kindMetKarakter = getKindMetKarakter(subKarakter);
+				
+				if(kindMetKarakter.karakter.contains(subKarakter)){
+					return kindMetKarakter.search(word.substring(1));
+				}else{
+					return data;
+				}
+			} catch (NullPointerException e) {
+				return data;
+			}	
+		}else{
+			return data;
+		}	
+	}
+
+	public void delete(String word) {
+		
+		
 	}
 	
-	
+
+
+
 		
 	
 }
